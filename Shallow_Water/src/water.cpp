96e208819fea4089eba2df01e7370148c4d07722
TiboHrqn <harquin.thibault@gmail.com>
2023-10-31 22:25:07 +0100

    definition textures 2d water

--- Shallow_Water/src/water.cpp
+++ Shallow_Water/src/water.cpp
@@ -2,15 +2,45 @@
 
 Water::Water()
 {
-	refractive_index = 1.333f;
-	absorbance = 100.0f;
-	color = vec3(0.0f, 0.0f, 1.0f);
-	m_shader_water = new ShaderGLSL("water_shader");
+    refractive_index = 1.333f;
+    absorbance = 100.0f;
+    color = vec3(0.0f, 0.0f, 1.0f);
+    m_shader_water = new ShaderGLSL("water_shader");
 
+    simulation_resolution = 512;
+
+    InitializeTextures(simulation_resolution);
 }
 
 void Water::load_shaders(std::string base_path)
 {
-	m_shader_water->compile_and_link_to_program();
-	ContextHelper::add_shader_to_hot_reload(m_shader_water);
+    m_shader_water->compile_and_link_to_program();
+    ContextHelper::add_shader_to_hot_reload(m_shader_water);
+}
+
+void Water::InitializeTextures(int size)
+{
+    //physics part
+    std::vector<glm::vec2> physicData(size * size, glm::vec2(0.0f, 0.0f));
+    glBindTexture(GL_TEXTURE_2D, texturePhysics);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RG32F, size, size, 0, GL_RG, GL_FLOAT, physicData.data());
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    //normals part
+    std::vector<glm::vec4> normalData(size * size, glm::vec4(0.0f, 1.0f, 0.0f, 0.0f));
+    glBindTexture(GL_TEXTURE_2D, textureNormals);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, size, size, 0, GL_RGBA, GL_FLOAT, normalData.data());
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Water::BindPhysicsTexture(GLuint unit)
+{
+    glActiveTexture(GL_TEXTURE0 + unit);
+    glBindTexture(GL_TEXTURE_2D, texturePhysics);
+}
+
+void Water::BindNormalsTexture(GLuint unit)
+{
+    glActiveTexture(GL_TEXTURE0 + unit);
+    glBindTexture(GL_TEXTURE_2D, textureNormals);
 }
--- Shallow_Water/src/water.hpp
+++ Shallow_Water/src/water.hpp
@@ -1,26 +1,34 @@
 #ifndef WATER_CLASS
 #define WATER_CLASS
 
-#include"buffer_structures.hpp"
+#include "buffer_structures.hpp"
 #include "helpers/helpers_common.hpp"
 #include <GLFW/glfw3.h>
+#include <vector>
 
 class Water {
-
 public:
-	Water();
-	void load_shaders(std::string base_path);
-	
+    Water();
+    void load_shaders(std::string base_path);
+
+    //creation des textures
+    void InitializeTextures(int size);
+
+    void BindPhysicsTexture(GLuint unit);
+    void BindNormalsTexture(GLuint unit);
 
 private:
+    ShaderGLSL* m_shader_water;
 
-	ShaderGLSL* m_shader_water;
+    int simulation_resolution;
 
-	Texture2D water_info[3]; //height, speed, normals;
+    // Ajout des textures
+    GLuint texturePhysics; //2 canaux: hauteur, vitesse
+    GLuint textureNormals; //4 canaux : 3 pour la normale, 1 unused
 
-	float refractive_index;
-	float absorbance;
-	vec3 color;
+    float refractive_index;
+    float absorbance;
+    vec3 color;
 };
 
 #endif
